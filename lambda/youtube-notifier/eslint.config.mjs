0933185b19f78715b376ffe6bb409917b2d82804
Tatsuro Terasaki <tatsuro.terasaki@gmail.com>
2025-10-04 17:10:08 +0900

    chore: add eslint

---
+++ lambda/youtube-notifier/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+      },
+      ecmaVersion: 2025,
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+])
